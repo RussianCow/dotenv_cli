5b1b1fd9415493bad102d085afe080372f9127fb
Sasha Chedygov <sasha@chedygov.com>
2018-04-13 16:31:58 -0700

    First commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod parser;
+
+use std::env;
+use std::path::{PathBuf};
+use std::process::Command;
+
+use parser::parse_file;
+
+
+fn main() {
+	let cwd = env::current_dir()
+		.expect("Could not get current working directory.");
+	let path: PathBuf = [cwd, ".env".into()].iter().collect();
+	let additional_env_vars = parse_file(&path);
+
+	let args: Vec<String> = env::args().collect();
+	let command_name = &args.get(1).expect("You must pass a command to run as an argument.");
+	let command_args = &args[2..];
+
+	let mut command = Command::new(command_name);
+	command.args(command_args);
+	for (key, value) in additional_env_vars.iter() {
+		command.env(key, value);
+	}
+	command.spawn()
+		.expect(&format!("Could not run the command: {}", command_name));
+}
---
+++ src/parser.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::{Path};
+
+
+pub fn parse_file(path: &Path) -> HashMap<String, String> {
+	let file = File::open(path)
+		.expect("There is no .env file in the current directory.");
+	let reader = BufReader::new(file);
+
+	let mut vars: HashMap<String, String> = HashMap::new();
+	for line in reader.lines() {
+		let maybe_key_value: Option<(String, String)> = {
+			let mut key = String::new();
+			let mut value = String::new();
+			let mut is_past_equal_sign = false;
+
+			for character in line.unwrap().chars() {
+				let is_first = key.is_empty();
+
+				// Skip comments
+				if is_first && character == '#' {
+					break
+				}
+
+				if !character.is_ascii() {
+					panic!("Non-ASCII character found: {}", character);
+				}
+				if is_first && character.is_numeric() {
+					panic!("The first character of an environment variable cannot be a number.");
+				}
+				if
+					!is_past_equal_sign
+					&& (
+						!character.is_numeric()
+						&& !character.is_uppercase()
+						&& character != '='
+					)
+				{
+					panic!("Invalid character in key name: {}", character);
+				}
+
+				if is_past_equal_sign {
+					value = value + &character.to_string();
+				} else {
+					if character == '=' {
+						is_past_equal_sign = true;
+					} else {
+						key = key + &character.to_string();
+					}
+				}
+			}
+
+			if key.is_empty() {
+				None
+			} else {
+				Some((key, value))
+			}
+		};
+
+		if maybe_key_value.is_none() {
+			continue
+		}
+		let (key, value) = maybe_key_value.unwrap();
+
+		vars.insert(key, value);
+	}
+
+	vars
+}
